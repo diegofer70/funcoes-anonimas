306597cc9470d690a14e0dbea17adf6279aae17d
Diego Araujo <ferreiradearaujod@gmail.com>
2024-07-04 11:46:55 -0300

    feat: adicionando funcoes anonimas

---
+++ script.js
@@ -0,0 +1,35 @@
+// function mensagem() {
+//     console.log('Essa mensagem aparece após 3 segundos!');
+// }
+
+// setTimeout(mensagem, 3000);
+
+// setTimeout(function(){
+//     console.log('Essa mensagem é mostrada após 3 segundos!');
+// }, 3000);
+
+// setTimeout(() => {
+//     console.log('Essa mensagem é mostrada após 3 segundos!');
+// }, 3000);
+
+const shoes = [
+    { nome: 'Nike', preco: 200, imagem: 'https://shoemix.fbitsstatic.net/img/p/tenis-casual-nike-air-max-intrlk-lit-branco-air-max-intrlk-lite-75448/291814-1.jpg?w=670&h=670&v=no-change&qs=ignore' },
+    { nome: 'Adidas', preco: 120, imagem: 'https://shoemix.fbitsstatic.net/img/p/tenis-casual-nike-air-max-intrlk-lit-branco-air-max-intrlk-lite-75448/291814-1.jpg?w=670&h=670&v=no-change&qs=ignore' },
+    { nome: 'Puma', preco: 100, imagem: 'https://shoemix.fbitsstatic.net/img/p/tenis-casual-nike-air-max-intrlk-lit-branco-air-max-intrlk-lite-75448/291814-1.jpg?w=670&h=670&v=no-change&qs=ignore'  },
+    { nome: 'Reebok', preco: 80, imagem: 'https://shoemix.fbitsstatic.net/img/p/tenis-casual-nike-air-max-intrlk-lit-branco-air-max-intrlk-lite-75448/291814-1.jpg?w=670&h=670&v=no-change&qs=ignore' },
+    { nome: 'Vans', preco: 60, imagem: 'https://shoemix.fbitsstatic.net/img/p/tenis-casual-nike-air-max-intrlk-lit-branco-air-max-intrlk-lite-75448/291814-1.jpg?w=670&h=670&v=no-change&qs=ignore'  },
+    { nome: 'Converse', preco: 40, imagem: 'https://shoemix.fbitsstatic.net/img/p/tenis-casual-nike-air-max-intrlk-lit-branco-air-max-intrlk-lite-75448/291814-1.jpg?w=670&h=670&v=no-change&qs=ignore'  },
+    { nome: 'Crocs', preco: 1, imagem: 'https://shoemix.fbitsstatic.net/img/p/tenis-casual-nike-air-max-intrlk-lit-branco-air-max-intrlk-lite-75448/291814-1.jpg?w=670&h=670&v=no-change&qs=ignore'  }
+]
+
+const shoesList = document.getElementById('shoes-list')
+
+shoes.forEach((shoe) => {
+    const li = document.createElement('li')
+    const img = document.createElement('img')
+    img.src = shoe.imagem
+    img.width = 100
+    li.textContent = `${shoe.nome} - $${shoe.preco}`
+    li.appendChild(img)
+    shoesList.appendChild(li)
+})
